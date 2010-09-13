5176b6baf45a3e3ecdcf81b159311b36c80e7268
Vivien Didelot <vivien.didelot@gmail.com>
2010-09-14 00:21:55 +1000

    fixed the simple-rss require problem and test cases issues

--- test/worldcat_test.rb
+++ test/worldcat_test.rb
@@ -4,24 +4,24 @@
 require 'test/unit'
 require 'worldcat'
 
-# I usually run test from the lib/ folder.
+# Note: I usually run test from the lib/ folder.
+
+# Ask the API key on standard input
+print "WorldCat API key: "
+WCKEY = STDIN.gets.strip
 
 class WorldCatTest < Test::Unit::TestCase
   def setup
-    @client = WorldCat.new
-
-    # Ask the API key on standard input
-    print "WorldCat API key: "
-    @client.api_key = STDIN.gets.strip
+    @client = WorldCat.new WCKEY
   end
 
-  def do_request_test
-    assert_raise WorldCatError do
+  def test_do_request
+    assert_raise WorldCat::WorldCatError do
       @client.open_search :wskey => ''
     end
   end
 
-  def open_search_test
+  def test_open_search
     # A search for civil war, returning a result with the default Atom format, starting position, and count
     atom = @client.open_search :query => "Civil War"
 
@@ -46,7 +46,7 @@ class WorldCatTest < Test::Unit::TestCase
     assert_equal "&lt;p class=\"citation_style_MLA\"&gt;Hemingway, Ernest. &lt;i&gt;For Whom the Bell Tolls&lt;/i&gt;. New York: Scribner, 1940. Print. &lt;/p&gt;", atom.entries.first.content
   end
 
-  def sru_search_test
+  def test_sru_search
     #marc = @client.sru_search :q => ""
   end
 end
